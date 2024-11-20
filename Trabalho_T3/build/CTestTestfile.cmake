9ec3543a3e1f7d847a432003c94f1d24ed84213c
Álisson Pereira Ferreira <113950309+alissonpef@users.noreply.github.com>
2024-11-20 16:41:41 -0300

    Delete Trabalho_T3 directory

--- Trabalho_T3/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Trabalho_ArvoreT3
-# Build directory: C:/Trabalho_ArvoreT3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
